e917465ac4d44dc442d9768e1c43600bc20946b4
amarrero7 <124218204+amarrero7@users.noreply.github.com>
2023-06-04 15:14:19 -0400

    intial coommit

---
+++ ContactsManagement/hubspot_manager.py
@@ -0,0 +1,25 @@
+import hubspot as hb
+import icloud
+import json
+from hubspot.crm.contacts import SimplePublicObjectInput
+from hubspot.crm.contacts.exceptions import ApiException
+
+# API
+api_client = hb.HubSpot(access_token='pat-na1-9112a282-25a9-4f47-886d-c3c4cfeac9e2')
+
+def create_contact(args):
+    try:
+        simple_public_object_input = SimplePublicObjectInput(
+            properties= {"phone": args['phone'] ,"firstname": args['name']}
+        )
+        api_response = api_client.crm.contacts.basic_api.create(
+            simple_public_object_input=simple_public_object_input
+        )
+    except ApiException as e:
+        print("Exception when creating contact: %s\n" % e)
+
+contacts_data = icloud.contacts()
+# test_contact = (contacts_data[0]['name'], contacts_data[0]['phone'])
+for contact in contacts_data:
+    create_contact(contact)
+
